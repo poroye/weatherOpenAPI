328ca25ff2883b7d94d1502cebd017beee485b85
tanat <31177223+poroye@users.noreply.github.com>
2021-09-07 20:28:29 -0700

    Initial Commit

---
+++ weatherOpenAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weatherOpenAPI
+//
+//  Created by ธนัท แสงเพิ่ม on 7/9/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weatherOpenAPI/weatherOpenAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherOpenAPIApp.swift
+//  weatherOpenAPI
+//
+//  Created by ธนัท แสงเพิ่ม on 7/9/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct weatherOpenAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
